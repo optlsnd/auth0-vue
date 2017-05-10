54bf65cebc838ea0dd31b41979a902ca6ad0946e
optlsnd <optlsnd@gmail.com>
2017-05-10 18:08:47 +0300

    bootstrap -> pure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+    <!--<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
           <span class="nav-item nav-link"><router-link to="/">Home</router-link></span>          
@@ -10,6 +10,23 @@
           <span class="nav-item nav-link navbar-right" v-if="authenticated" @click="logout">Log out</span>                
         </div>
         <span class="nav-item nav-link pull-right">{{user.name}}<img class="userpic" :src="user.picture" /></span>
+      </div>
+    </nav>-->
+    <nav class="pure-menu pure-menu-horizontal">
+      <div class="pure-g">
+        <div class="pure-u-2-3">
+          <a href="#" class="pure-menu-heading pure-menu-link">Oauth0 test app</a>
+          <ul class="pure-menu-list">
+              <li class="pure-menu-item"><a href="/" class="pure-menu-link">Home</a></li>
+              <li class="pure-menu-item"><a href="/public" class="pure-menu-link">Public</a></li>
+              <li class="pure-menu-item"><a href="/private" class="pure-menu-link" v-if="authenticated">Private</a></li>
+              <li class="pure-menu-item"><a href="#" class="pure-menu-link" v-if="!authenticated" @click.prevent="login">Log in</a></li>
+              <li class="pure-menu-item"><a href="#" class="pure-menu-link" v-if="authenticated" @click.prevent="logout">Log out</a></li>
+          </ul>
+        </div>
+        <div class="pure-u-1-3 user-info" v-if="authenticated">
+          <span>{{user.name}}<img class="userpic" :src="user.picture" /></span>
+        </div>
       </div>
     </nav>
     <router-view></router-view>
@@ -25,8 +42,8 @@ export default {
   data () {
     return {
       authenticated: false,
-      secretThing: '_kYPhunoANsiBcoPcb5I6jKZ5FSp3YR3nKZsqnppPxP3-f4kzc4EHMVbfuZlt0qo',
-      lock: new Auth0Lock('Y2cYqxQihLIa5CDAKCNImwBjZPzZ7GBt', 'optlsnd.auth0.com'),
+      secretThing: 'krPnBiWLJlDRAupMl4yTfS7ppqU6JTGBUU86Abiq4qsg59T5YdrgUdAd9IdrlGu0',
+      lock: new Auth0Lock('gX5Z7mAi8fzcl5T2hkHMs5zmQK1ewa0e', 'damnedweb.auth0.com'),
       user: {}
     }
   },
@@ -80,7 +97,14 @@ a {
   margin: 0 10px;
 }
 
+.user-info {
+  padding: .5rem 0;
+  text-align: right;
+}
+
 .userpic {
   border-radius: 50%;
+  width: 2rem;
+  height: 2rem;
 }
 </style>
